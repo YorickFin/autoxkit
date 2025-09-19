b8628a2ea3fde7f74b0e16a2e7cf47848b0fd861
YorickFin <1582456060@qq.com>
2025-09-20 00:11:35 +0800

    fix: 修复 autoxkit/mousekey/__init__.py 中的 import 错误 style: 优化 autoxkit/__init__.py 中的代码格式

--- autoxkit/__init__.py
+++ autoxkit/__init__.py
@@ -1,1 +1,30 @@
-from .mousekey import *
+from .mousekey import (
+    left_down, left_up, left_click,
+    right_down, right_up, right_click,
+    middle_down, middle_up, middle_click,
+    side1_down, side1_up, side1_click,
+    side2_down, side2_up, side2_click,
+    move_relative, move_absolute, wheel_scroll,
+
+    key_down, key_up, key_click, key_combination,
+
+    start_listening, stop_listening, set_event_handlers,
+    get_mouse_position, KeyEvent, MouseEvent
+)
+
+__all__ = [
+    # 鼠标操作
+    "left_down", "left_up", "left_click",
+    "right_down", "right_up", "right_click",
+    "middle_down","middle_up","middle_click",
+    "side1_down","side1_up","side1_click",
+    "side2_down","side2_up","side2_click",
+    "move_relative","move_absolute", "wheel_scroll",
+
+    # 键盘操作
+    "key_down", "key_up", "key_click", "key_combination",
+
+    # 鼠标键盘钩子
+    "start_listening","stop_listening","set_event_handlers",
+    "get_mouse_position", "KeyEvent", "MouseEvent"
+]
